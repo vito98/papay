990712ac87fdcd312a3a2ae9b4b7049b1eee66a4
vito98 <abror.qodirov.97@gmail.com>
2023-02-22 01:07:28 +0900

    task has done

--- train.js
+++ train.js
@@ -1,30 +1,43 @@
 console.log("train js");
 
-class Member {
-  #counts;
-  constructor(counts) {
-    this.#counts = counts;
-  }
-
-  addMember(amount) {
-    this.#counts += amount;
-  }
-
-  removeMember(amount) {
-    this.#counts -= amount;
-  }
-
-  inform() {
-    console.log(this.#counts);
-  }
+function findDays(initial_day, last_day) {
+  return Math.round((last_day - initial_day) / (1000 * 60 * 60 * 24));
 }
 
-const new_member = new Member(0);
+function findDate(nums) {
+  let presentDay = nums.split("/");
+  return new Date(presentDay[2], presentDay[0] - 1, presentDay[1]);
+}
 
-new_member.addMember(7);
-new_member.removeMember(3);
+console.log(findDays(findDate("12/05/2023"), findDate("12/07/2023")));
 
-new_member.inform();
+/************************************************/
+
+// class Member {
+//   #counts;
+//   constructor(counts) {
+//     this.#counts = counts;
+//   }
+
+//   addMember(amount) {
+//     this.#counts += amount;
+//   }
+
+//   removeMember(amount) {
+//     this.#counts -= amount;
+//   }
+
+//   inform() {
+//     console.log(this.#counts);
+//   }
+// }
+
+// const new_member = new Member(0);
+
+// new_member.addMember(7);
+// new_member.removeMember(3);
+
+// new_member.inform();
 
 // function calculate(nums) {
 //   let result = eval(nums);
