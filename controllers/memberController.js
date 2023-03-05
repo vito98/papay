ffc8e915ec66d78b66488e5817898863cbc23c54
vito98 <abror.qodirov.97@gmail.com>
2023-03-05 13:11:11 +0900

    feat: jwt and cookie creation

--- controllers/memberController.js
+++ controllers/memberController.js
@@ -1,6 +1,9 @@
 const Member = require("../models/Member");
+const assert = require("assert");
+const Definer = require("../lib/mistake");
 
 let memberController = module.exports;
+const jwt = require("jsonwebtoken");
 
 memberController.signup = async (req, res) => {
   try {
@@ -9,7 +12,11 @@ memberController.signup = async (req, res) => {
       member = new Member(),
       new_member = await member.signupData(data);
 
-    // TODO: AUTHENTICATE BASED ON JWT
+    const token = memberController.createToken(new_member);
+    res.cookie("access_token", token, {
+      maxAge: 6 * 3600 * 1000,
+      httpOnly: true,
+    });
 
     res.json({ state: "succeed", data: new_member });
   } catch (err) {
@@ -25,6 +32,12 @@ memberController.login = async (req, res) => {
       member = new Member(),
       result = await member.loginData(data);
 
+    const token = memberController.createToken(result);
+    res.cookie("access_token", token, {
+      maxAge: 6 * 3600 * 1000,
+      httpOnly: true,
+    });
+
     res.json({ state: "succeed", data: result });
   } catch (err) {
     console.log(`ERROR, cont/login, ${err.message}`);
@@ -33,6 +46,40 @@ memberController.login = async (req, res) => {
 };
 
 memberController.logout = (req, res) => {
-  console.log("GET cont.logout");
+  console.log("GET cont/logout");
   res.end("logout sahifasidasiz");
+};
+
+memberController.createToken = (result) => {
+  try {
+    const upload_data = {
+      _id: result._id,
+      mb_nick: result.mb_nick,
+      mb_type: result.mb_type,
+    };
+
+    const token = jwt.sign(upload_data, process.env.SECRET_TOKEN, {
+      expiresIn: "6h",
+    });
+    assert.ok(token, Definer.auth_err2);
+
+    return token;
+  } catch (err) {
+    throw err;
+  }
+};
+
+memberController.checkMyAuthentication = (req, res) => {
+  try {
+    console.log("POST: cont/checkMyAuthentication");
+    let token = req.cookies["access_token"];
+    console.log("token::", token);
+
+    const member = token ? jwt.verify(token, process.env.SECRET_TOKEN) : null;
+    assert.ok(token, Definer.auth_err2);
+
+    res.json({ state: "succeed", data: member });
+  } catch (err) {
+    throw err;
+  }
 };
